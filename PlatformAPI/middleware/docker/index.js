ba4eb70876ef32ac3345f5fe1e29383bc9b4cc9e
Vitaly <smolvitos@gnail.com>
2019-05-13 22:06:49 +0300

    Adding listing of services

--- PlatformAPI/middleware/docker/index.js
+++ PlatformAPI/middleware/docker/index.js
@@ -1,4 +1,4 @@
-//Сбор информации от сервиса Docker и из БД. Связь
+//Сбор информации от сервиса Docker и из БД
 const { Docker } = require('node-docker-api')
 
 const docker = new Docker({ socketPath: '/var/run/docker.sock' })
@@ -15,7 +15,7 @@ module.exports.listServices = () => (req, res) => {
 				return {
 				  	containerName: null,
 				  	baseImage: RepoTags[0],
-				  	status: null,
+				  	state: null,
 				  	ports: null
 			  	}
 			})
@@ -23,118 +23,31 @@ module.exports.listServices = () => (req, res) => {
 		.then((dockerServices) => {
 			dockerServicesGlobal = dockerServices
 		})
-		.then(() => {
-			return docker.container.list({all: true})
-			.then((containers) => {
-				Promise.all(containers.map((container) => {
-					return container.status()
-					.then((containerInfo) => {
-						let  { Name } = containerInfo.data
-			  				,{ Image } = containerInfo.data.Config
-			  				,{ Status } = containerInfo.data.State
-			  				,{ Ports } = containerInfo.data.NetworkSettings
-			  			
-			  			for (let dockerService of dockerServicesGlobal) {
-			  				//console.log(dockerService)
-			  				//console.log(Image)
-			  				if (dockerService['baseImage'].split(':')[0] == Image) {
-			  					return {
-			  						containerName: Name,
-								  	baseImage: Image,
-								  	status: Status,
-								  	ports: Ports
-			  					}
-			  				} else {
-			  					return dockerService
-			  				}
-			  			}
-					})
-				}))
-				.then((servicesForClient) => {
-					console.log(servicesForClient)
-				})
+	})
+	.then(() => {
+		docker.container.list({all: true})
+		.then((containers) => {
+			Promise.all(dockerServicesGlobal.map((dockerService) => {
+				for (let container of containers) {
+					console.log(container)
+					let  { Names, Image, State, Ports } = container.data
+			  		console.log(container.data.NetworkSettings.Networks)
+					if (dockerService['baseImage'].split(':')[0] == Image) {
+						console.log(`${Names[0]} ${Image} ${State} ${Ports}`)
+						return {
+							containerName: Names[0],
+				  			baseImage: Image,
+				  			state: State,
+				  			ports: Ports
+						}
+					}
+				}
+				return dockerService
+			}))
+			.then((servicesForClient) => {
+				res.json(servicesForClient)
 			})
 		})
 	})
 	.catch((error) => console.log(error))
 }
-
-		/*return new Promise((resolve, reject) => {
-			let dockerServices = []
-			images.forEach((image, number) => {
-				image.status()
-				.then((imageInfo) => {
-					//console.log(imageInfo)
-					let  { RepoTags } = imageInfo.data
-				
-					let dockerService = {
-				  			containerName: null,
-				  			baseImage: RepoTags[0],
-				  			status: null,
-				  			ports: null
-			  		}
-			  		//console.log(dockerServices)
-			  		dockerServices.push(dockerService)
-			  		if (images.length - 1 == number) {
-			  			//console.log(dockerServices)
-			  			resolve(dockerServices)
-			  		}
-				})			
-			})
-		})
-	})
-	.then((dockerServices) => {
-		//console.log(dockerServices) ok
-		docker.container.list({all: true})
-		.then((containers) => {
-			containers.forEach((container, number) => {
-			container.status()
-			.then((containerInfo) => {
-				let  { Name } = containerInfo.data
-			  		,{ Image } = containerInfo.data.Config
-			  		,{ Status } = containerInfo.data.State
-			  		,{ Ports } = containerInfo.data.NetworkSettings
-			  	//console.log(dockerServices)
-			  	//console.log(typeof tools)
-			  	console.log(tools.checkForUpdate(dockerServices, { baseImage: Image + ':latest' }))
-			  	//console.log('serviceAsContainer')
-
-			  	//изменение объекта dockerServices
-			  	/*let dockerService = {
-		  			containerName: Name,
-		  			baseImage: Image,
-		  			status: Status,
-		  			ports: Ports
-	  			}
-	  			console.log(dockerService) //только информация от Docker
-			})
-			
-			})
-		})
-	})*/
-
-	/*docker.container.list({all: true})
-	.then((containers) => {
-		containers.forEach((container, number) => {
-			container.status()
-			.then((containerInfo) => {
-				let  { Name } = containerInfo.data
-			  		,{ Image } = containerInfo.data.Config
-			  		,{ Status } = containerInfo.data.State
-			  		,{ Ports } = containerInfo.data.NetworkSettings
-			  	
-			  	let dockerService = {
-		  			containerName: Name,
-		  			baseImage: Image,
-		  			status: Status,
-		  			ports: Ports
-	  			}
-	  			console.log(dockerService) //только информация от Docker
-			})
-			
-		})
-	})
-	.catch((error) => console.log(error))*/
-// images[1].data.RepoTags
-// images - Array
-// RepoTags - Array
